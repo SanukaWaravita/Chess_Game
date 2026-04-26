d33570cc44750e5bdf6887928a449f4b1499b156
SanukaWaravita <sanukawaravita@gmail.com>
2026-04-26 17:21:39 +0530

    Add chess piece image mapping

---
+++ chess_game/frontend/src/data/pieceImages.js
@@ -0,0 +1,15 @@
+export const pieceImages = {
+  P: '/pieces/white-pawn.svg',
+  R: '/pieces/white-rook.svg',
+  N: '/pieces/white-knight.svg',
+  B: '/pieces/white-bishop.svg',
+  Q: '/pieces/white-queen.svg',
+  K: '/pieces/white-king.svg',
+
+  p: '/pieces/black-pawn.svg',
+  r: '/pieces/black-rook.svg',
+  n: '/pieces/black-knight.svg',
+  b: '/pieces/black-bishop.svg',
+  q: '/pieces/black-queen.svg',
+  k: '/pieces/black-king.svg',
+}
